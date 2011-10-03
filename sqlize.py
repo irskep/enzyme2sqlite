eab8bb83dc72ce2c4ac820f4e6c84322c7837f88
Stephen Johnson <srj15@case.edu>
2011-10-03 01:17:48 -0400

    Successful serialization and deserialization of database

--- sqlize.py
+++ sqlize.py
@@ -8,18 +8,31 @@ EnzymeRow = namedtuple('EnzymeRow',
                        'cofactors', 'comments', 'prosite_ref', 'db_ref'])
 
 
-identity = lambda x: x
-string_join = lambda l: '-!-'.join(l)
+bang_join = lambda l: '-!-'.join(l)
 db_ref_join = lambda r: ';'.join(','.join(x for x in item) for item in r)
 CONVERT_FUNCS = {
-    'id':                   identity,
-    'name':                 string_join,
-    'alt_name':             string_join,
-    'catalytic_activity':   string_join,
-    'cofactors':            string_join,
-    'comments':             string_join,
-    'prosite_ref':          string_join,
+    'id':                   lambda x: x,
+    'name':                 bang_join,
+    'alt_name':             bang_join,
+    'catalytic_activity':   bang_join,
+    'cofactors':            bang_join,
+    'comments':             bang_join,
+    'prosite_ref':          lambda l: ';'.join(l),
     'db_ref':               db_ref_join,
+}
+
+
+bang_split = lambda s: s.split('-!-')
+db_ref_split = lambda s: [[x for x in item.split(',')] for item in s.split(';')]
+UNCONVERT_FUNCS = {
+    'id':                   lambda x: x,
+    'name':                 bang_split,
+    'alt_name':             bang_split,
+    'catalytic_activity':   bang_split,
+    'cofactors':            bang_split,
+    'comments':             bang_split,
+    'prosite_ref':          lambda s: s.split(';'),
+    'db_ref':               db_ref_split,
 }
 
 
@@ -41,3 +54,8 @@ def sqlize(data, out):
 
     conn.commit()
     c.close()
+    return conn
+
+
+def desqlize_row(row):
+    return {k: UNCONVERT_FUNCS[k](v) for k, v in EnzymeRow(*row)._asdict().items()}
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import tempfile
 
 from enzyme2sqlite import *
 from parse import *
@@ -44,44 +45,54 @@ def test_parse_semicolon():
                 ['P28469', 'ADH1A_MACMU'],
                 ['Q5RBP7', 'ADH1A_PONAB']])
 
+test_item_1_1_1_2 = {
+    'catalytic_activity': ['An alcohol + NADP(+) = an aldehyde + NADPH.'],
+    'name': ['Alcohol dehydrogenase (NADP(+)).'],
+    'prosite_ref': ['PDOC00061'],
+    'db_ref': [['Q6AZW2', 'A1A1A_DANRE'],
+               ['Q568L5', 'A1A1B_DANRE'],
+               ['P35630', 'ADH1_ENTHI'],
+               ['Q24857', 'ADH3_ENTHI'],
+               ['Q04894', 'ADH6_YEAST'],
+               ['P25377', 'ADH7_YEAST'],
+               ['O57380', 'ADH8_RANPE'],
+               ['P0CH36', 'ADHC1_MYCS2'],
+               ['P0CH37', 'ADHC2_MYCS2'],
+               ['P0A4X1', 'ADHC_MYCBO'],
+               ['P0A4X0', 'ADHC_MYCTU'],
+               ['P25984', 'ADH_CLOBE'],
+               ['P75214', 'ADH_MYCPN'],
+               ['P14941', 'ADH_THEBR'],
+               ['Q3ZCJ2', 'AK1A1_BOVIN'],
+               ['Q5ZK84', 'AK1A1_CHICK'],
+               ['O70473', 'AK1A1_CRIGR'],
+               ['P14550', 'AK1A1_HUMAN'],
+               ['Q9JII6', 'AK1A1_MOUSE'],
+               ['P50578', 'AK1A1_PIG'],
+               ['Q5R5D5', 'AK1A1_PONAB'],
+               ['P51635', 'AK1A1_RAT'],
+               ['Q6GMC7', 'AK1A1_XENLA'],
+               ['Q28FD1', 'AK1A1_XENTR'],
+               ['Q9UUN9', 'ALD2_SPOSA'],
+               ['P27800', 'ALDX_SPOSA']],
+    'cofactors': ['Zinc.'],
+    'id': '1.1.1.2',
+    'comments': [
+        'Some members of this group oxidize only primary alcohols; others act also on secondary alcohols.',
+        'May be identical with EC 1.1.1.19, EC 1.1.1.33 and EC 1.1.1.55.',
+        'A-specific with respect to NADPH.'],
+    'alt_name': ['Aldehyde reductase (NADPH).']
+}
+
 def test_all_parsing():
     with open(DAT_FILE, 'r') as f:
         data = parse(f)
-        assert data['1.1.1.2'] == {
-            'catalytic_activity': ['An alcohol + NADP(+) = an aldehyde + NADPH.'],
-            'name': ['Alcohol dehydrogenase (NADP(+)).'],
-            'prosite_ref': ['PDOC00061'],
-            'db_ref': [['Q6AZW2', 'A1A1A_DANRE'],
-                       ['Q568L5', 'A1A1B_DANRE'],
-                       ['P35630', 'ADH1_ENTHI'],
-                       ['Q24857', 'ADH3_ENTHI'],
-                       ['Q04894', 'ADH6_YEAST'],
-                       ['P25377', 'ADH7_YEAST'],
-                       ['O57380', 'ADH8_RANPE'],
-                       ['P0CH36', 'ADHC1_MYCS2'],
-                       ['P0CH37', 'ADHC2_MYCS2'],
-                       ['P0A4X1', 'ADHC_MYCBO'],
-                       ['P0A4X0', 'ADHC_MYCTU'],
-                       ['P25984', 'ADH_CLOBE'],
-                       ['P75214', 'ADH_MYCPN'],
-                       ['P14941', 'ADH_THEBR'],
-                       ['Q3ZCJ2', 'AK1A1_BOVIN'],
-                       ['Q5ZK84', 'AK1A1_CHICK'],
-                       ['O70473', 'AK1A1_CRIGR'],
-                       ['P14550', 'AK1A1_HUMAN'],
-                       ['Q9JII6', 'AK1A1_MOUSE'],
-                       ['P50578', 'AK1A1_PIG'],
-                       ['Q5R5D5', 'AK1A1_PONAB'],
-                       ['P51635', 'AK1A1_RAT'],
-                       ['Q6GMC7', 'AK1A1_XENLA'],
-                       ['Q28FD1', 'AK1A1_XENTR'],
-                       ['Q9UUN9', 'ALD2_SPOSA'],
-                       ['P27800', 'ALDX_SPOSA']],
-            'cofactors': ['Zinc.'],
-            'id': '1.1.1.2',
-            'comments': [
-                'Some members of this group oxidize only primary alcohols; others act also on secondary alcohols.',
-                'May be identical with EC 1.1.1.19, EC 1.1.1.33 and EC 1.1.1.55.',
-                'A-specific with respect to NADPH.'],
-            'alt_name': ['Aldehyde reductase (NADPH).']
-        }
+        assert data['1.1.1.2'] == test_item_1_1_1_2
+
+def test_sqlize():
+    with open(DAT_FILE, 'r') as f:
+        data = parse(f)
+        conn = sqlize(data, ':memory:')
+        c = conn.cursor()
+        for row in c.execute('select * from enzymes where id=?', ('1.1.1.2',)):
+            assert desqlize_row(row) == test_item_1_1_1_2
